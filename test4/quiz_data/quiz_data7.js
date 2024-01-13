be25134ddfb63bafdff2d2e0cd7e925eda3264a5
AmanoTakumi00 <156516183+AmanoTakumi00@users.noreply.github.com>
2024-01-13 22:29:22 +0900

    Add files via upload

---
+++ test4/quiz_data/quiz_data7.js
@@ -0,0 +1,67 @@
+/*quiz_data7 */
+
+const questions = [
+    {
+//539問目
+question:"teach(動詞)の意味は？",
+choices: ["教える","道","似ている","期間"],
+correctAnswer: "教える"
+    },
+    {
+//544問目
+question:"trade(名詞)の意味は？",
+choices: ["スタッフ","食べ物","願う","交換する"],
+correctAnswer: "交換する"
+    },
+    {
+//550問目
+question:"focus(名詞)の意味は？",
+choices: ["焦点","事務所","関係","売る"],
+correctAnswer: "焦点"
+    },
+    {
+//553問目
+question:"instead(副詞)の意味は？",
+choices: ["代わり","最上部","行動","横切って"],
+correctAnswer: "代わり"
+    },
+    {
+//557問目
+question:"single(形容詞)の意味は？",
+choices: ["近く","生産する","クラス","単独"],
+correctAnswer: "単独"
+    },
+    {
+//565問目
+question:"choice(名詞)の意味は？",
+choices: ["瞬間","体","期間","選択"],
+correctAnswer: "選択"
+    },
+    {
+//572問目
+question:"limit(名詞)の意味は？",
+choices: ["必要とする","強いる","確実な","限度"],
+correctAnswer: "限度"
+    },
+    {
+//577問目
+question:"effort(名詞)の意味は？",
+choices: ["努力","残る","主題","改善する"],
+correctAnswer: "努力"
+    },
+    {
+//582問目
+question:"lie(動詞)の意味は？",
+choices: ["横になる","外","力","～の上に"],
+correctAnswer: "横になる"
+    },
+    {
+//591問目
+question:"century(名詞)の意味は？",
+choices: ["町","耐える、負う","世紀","個人的な"],
+correctAnswer: "世紀"
+    }
+];
+
+export default questions;
+
